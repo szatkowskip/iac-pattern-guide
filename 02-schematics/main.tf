30c14365dfb511a3294c25690f96119118e611a0
szatkowskip <70375898+szatkowskip@users.noreply.github.com>
2024-03-26 11:48:16 +0100

    Update main.tf

--- 02-schematics/main.tf
+++ 02-schematics/main.tf
@@ -25,7 +25,7 @@ resource "ibm_is_instance" "iac_test_instance" {
 
   user_data = <<-EOUD
               #!/bin/bash
-              echo "Hello Beautiful World" > index.html
+              echo "Hello!" > index.html
               nohup busybox httpd -f -p ${var.port} &
               EOUD
 
